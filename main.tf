464a4d341b4765dc2c969f57712b2e8bb72b20cf
campus\ch <christian.sauer@stud.fh-campuswien.ac.at>
2024-10-07 19:55:06 +0200

    Assigment3: inital terraform setup

---
+++ main.tf
@@ -0,0 +1,34 @@
+resource "azurerm_resource_group" "example" {
+  name     = "Devops-terraform"
+  location = "West Europe"
+}
+
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = "acctest-01"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_container_app_environment" "example" {
+  name                       = "Example-Environment"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+}
+resource "azurerm_container_app" "example" {
+  name                         = "example-app"
+  container_app_environment_id = azurerm_container_app_environment.example.id
+  resource_group_name          = azurerm_resource_group.example.name
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = "examplecontainerapp"
+      image  = "csauercampus/devops-nextjs:main"
+      cpu    = 0.25
+      memory = "0.5Gi"
+    }
+  }
+}
